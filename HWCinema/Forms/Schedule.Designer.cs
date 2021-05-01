33e831f5d90e131f5ed7a9720bb6a181e6405db0
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-01 15:03:01 +0300

    Add CountFilms

--- HWCinema/Forms/Schedule.Designer.cs
+++ HWCinema/Forms/Schedule.Designer.cs
@@ -29,12 +29,57 @@ namespace HWCinema.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.NameMovie = new System.Windows.Forms.ComboBox();
+            this.CountFilms = new System.Windows.Forms.NumericUpDown();
+            this.TimeFilm = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CountFilms)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NameMovie
+            // 
+            this.NameMovie.FormattingEnabled = true;
+            this.NameMovie.Location = new System.Drawing.Point(193, 69);
+            this.NameMovie.Name = "NameMovie";
+            this.NameMovie.Size = new System.Drawing.Size(286, 21);
+            this.NameMovie.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.CountFilms.Location = new System.Drawing.Point(74, 71);
+            this.CountFilms.Name = "numericUpDown1";
+            this.CountFilms.Size = new System.Drawing.Size(56, 20);
+            this.CountFilms.TabIndex = 1;
+            this.CountFilms.ValueChanged += new System.EventHandler(this.CountFilms_ValueChanged);
+            // 
+            // TimeFilm
+            // 
+            this.TimeFilm.Location = new System.Drawing.Point(509, 69);
+            this.TimeFilm.Name = "TimeFilm";
+            this.TimeFilm.Size = new System.Drawing.Size(120, 20);
+            this.TimeFilm.TabIndex = 2;
+            this.TimeFilm.Text = "00";
+            this.TimeFilm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Schedule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TimeFilm);
+            this.Controls.Add(this.CountFilms);
+            this.Controls.Add(this.NameMovie);
+            this.Name = "Schedule";
             this.Text = "Schedule";
+            ((System.ComponentModel.ISupportInitialize)(this.CountFilms)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox NameMovie;
+        private System.Windows.Forms.NumericUpDown CountFilms;
+        private System.Windows.Forms.TextBox TimeFilm;
     }
 }
--- HWCinema/Forms/Schedule.cs
+++ HWCinema/Forms/Schedule.cs
@@ -12,9 +12,34 @@ namespace HWCinema.Forms
 {
     public partial class Schedule : Form
     {
+        private int _countFilms;
         public Schedule()
         {
             InitializeComponent();
+            _countFilms = 0;
+        }
+
+        private void CountFilms_ValueChanged(object sender, EventArgs e)
+        {
+            if (CountFilms.Value > _countFilms)
+            {
+                object[] count = new object[(int)CountFilms.Value - _countFilms];
+                for (int i = 0; i < count.Length; i++)
+                {
+                    count[i] = new object();
+                }
+                NameMovie.Items.AddRange(count);
+                _countFilms = (int)CountFilms.Value;
+            }
+            else if(CountFilms.Value < _countFilms)
+            {
+                int tmp = _countFilms - (int)CountFilms.Value;
+                for (int i = 0; i < tmp; i++)
+                {
+                    NameMovie.Items.RemoveAt(NameMovie.Items.Count - 1);
+                }
+                _countFilms = (int)CountFilms.Value;
+            }
         }
     }
 }
