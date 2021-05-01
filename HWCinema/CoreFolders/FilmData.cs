10cf93493b5f7f230768b9712be2922e45a1a43b
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-01 15:59:22 +0300

    FilmData

---
+++ HWCinema/CoreFolders/FilmData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWCinema.CoreFolders
+{
+    public class FilmData
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+    }
+}
