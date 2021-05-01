44584c9369a4a6fbb7c8d644e60d46591824ec1c
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-01 14:26:57 +0300

    Add test Remove Hall

--- HWCinema.Tests/HallManagement.Tests/HallsManagementTests.cs
+++ HWCinema.Tests/HallManagement.Tests/HallsManagementTests.cs
@@ -14,5 +14,12 @@ namespace HWCinema.Tests.HallsManagement.Tests
             actual.Add(hall);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCaseSource(typeof(RemoveHallsTestSource))]
+        public void RemovingHall_WhenAdding_GeneralListHalls(List<Hall> actual, Hall hall, List<Hall> expected)
+        {
+            actual.Remove(hall);
+            Assert.AreEqual(expected, actual);
+        }
     }    
 }
