c984bd2efaabc064a8c5745c33c2af5b0449463d
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-01 13:06:15 +0300

    Serialize

--- HWCinema/Serelization/Json/Serialize.cs
+++ HWCinema/Serelization/Json/Serialize.cs
@@ -9,6 +9,14 @@ namespace HWCinema.Serelization.Json
 {
     public class Serialize
     {
-        
+        private StructSettings _setting = new StructSettings();
+        public string Serializer()
+        {
+            return JsonConvert.SerializeObject(_setting, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                });
+        }
     }
 }
