79b8cd6f8a4671bd44fbc4b5d6d2eaf494917035
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-01 14:20:20 +0300

    Add Hall Test

---
+++ HWCinema.Tests/HallManagement.Tests/HallsManagementTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using System.Collections;
+using HWCinema.CoreFolders;
+using System.Collections.Generic;
+using HWCinema.Tests.HallManagement.Tests;
+
+namespace HWCinema.Tests.HallsManagement.Tests
+{
+    public class HallsManagementTests
+    {
+        [TestCaseSource(typeof(AddHallsTestSource))]
+        public void AddingHall_WhenAdding_GeneralListHalls(List<Hall> actual, Hall hall, List<Hall> expected)
+        {
+            actual.Add(hall);
+            Assert.AreEqual(expected, actual);
+        }
+    }    
+}
