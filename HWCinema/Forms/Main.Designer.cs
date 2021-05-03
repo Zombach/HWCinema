dcb14bde86b14b9f1fb1e59c883b8ac851feafc0
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-03 20:50:56 +0300

    Fix

--- HWCinema/Forms/Main.Designer.cs
+++ HWCinema/Forms/Main.Designer.cs
@@ -29,71 +29,50 @@ namespace HWCinema.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.ManagementHalls = new System.Windows.Forms.Button();
-            this.View = new System.Windows.Forms.Button();
             this.Loading = new System.Windows.Forms.Button();
             this.Saving = new System.Windows.Forms.Button();
-            this.MakeSchedule = new System.Windows.Forms.Button();
+            this.CreateSchedule = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // ManagementHalls
-            // 
-            this.ManagementHalls.Location = new System.Drawing.Point(66, 62);
-            this.ManagementHalls.Name = "ManagementHalls";
-            this.ManagementHalls.Size = new System.Drawing.Size(200, 80);
-            this.ManagementHalls.TabIndex = 0;
-            this.ManagementHalls.Text = "Управленние залами";
-            this.ManagementHalls.UseVisualStyleBackColor = true;
-            this.ManagementHalls.Click += new System.EventHandler(this.Create_Click);
-            // 
-            // View
-            // 
-            this.View.Location = new System.Drawing.Point(314, 62);
-            this.View.Name = "View";
-            this.View.Size = new System.Drawing.Size(200, 80);
-            this.View.TabIndex = 1;
-            this.View.Text = "Отобразить расписание";
-            this.View.UseVisualStyleBackColor = true;
-            this.View.Click += new System.EventHandler(this.View_Click);
             // 
             // Loading
             // 
-            this.Loading.Location = new System.Drawing.Point(314, 234);
+            this.Loading.Location = new System.Drawing.Point(44, 168);
             this.Loading.Name = "Loading";
             this.Loading.Size = new System.Drawing.Size(200, 80);
             this.Loading.TabIndex = 2;
             this.Loading.Text = "Загрузить расписание";
             this.Loading.UseVisualStyleBackColor = true;
+            this.Loading.Click += new System.EventHandler(this.Loading_Click);
             // 
             // Saving
             // 
-            this.Saving.Location = new System.Drawing.Point(66, 234);
+            this.Saving.Location = new System.Drawing.Point(312, 168);
             this.Saving.Name = "Saving";
             this.Saving.Size = new System.Drawing.Size(200, 80);
             this.Saving.TabIndex = 3;
             this.Saving.Text = "Выгрузить расписание";
             this.Saving.UseVisualStyleBackColor = true;
+            this.Saving.Click += new System.EventHandler(this.Saving_Click);
             // 
-            // MakeSchedule
+            // CreateSchedule
             // 
-            this.MakeSchedule.Location = new System.Drawing.Point(221, 167);
-            this.MakeSchedule.Name = "MakeSchedule";
-            this.MakeSchedule.Size = new System.Drawing.Size(148, 48);
-            this.MakeSchedule.TabIndex = 4;
-            this.MakeSchedule.Text = "Составить расписание";
-            this.MakeSchedule.UseVisualStyleBackColor = true;
-            this.MakeSchedule.Click += new System.EventHandler(this.MakeSchedule_Click);
+            this.CreateSchedule.Location = new System.Drawing.Point(182, 36);
+            this.CreateSchedule.Name = "CreateSchedule";
+            this.CreateSchedule.Size = new System.Drawing.Size(200, 80);
+            this.CreateSchedule.TabIndex = 4;
+            this.CreateSchedule.Text = "Составить расписание";
+            this.CreateSchedule.UseVisualStyleBackColor = true;
+            this.CreateSchedule.Click += new System.EventHandler(this.CreateSchedule_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 353);
-            this.Controls.Add(this.MakeSchedule);
+            this.Controls.Add(this.CreateSchedule);
             this.Controls.Add(this.Saving);
             this.Controls.Add(this.Loading);
-            this.Controls.Add(this.View);
-            this.Controls.Add(this.ManagementHalls);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Name = "Main";
             this.Text = "Main";
             this.ResumeLayout(false);
@@ -101,11 +80,8 @@ namespace HWCinema.Forms
         }
 
         #endregion
-
-        private System.Windows.Forms.Button ManagementHalls;
-        private System.Windows.Forms.Button View;
         private System.Windows.Forms.Button Loading;
         private System.Windows.Forms.Button Saving;
-        private System.Windows.Forms.Button MakeSchedule;
+        private System.Windows.Forms.Button CreateSchedule;
     }
 }
